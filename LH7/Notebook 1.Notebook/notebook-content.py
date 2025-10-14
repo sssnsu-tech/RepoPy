3e882fba4ffb4a67adf3ab8804d0986c208f208c
sssnsu-tech <sssnsu@gmail.com>
2025-10-14 10:33:20 +0300

    LH4

---
+++ LH7/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,91 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "0343156d-43b4-4187-be43-c658a0dfd404",
+# META       "default_lakehouse_name": "LH9",
+# META       "default_lakehouse_workspace_id": "435bef55-2c35-40fd-9667-dcf68f447683",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "0343156d-43b4-4187-be43-c658a0dfd404"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.types import StructType, IntegerType, StringType, DoubleType
+
+# define the schema
+schema = StructType() \
+.add("ProductID", IntegerType(), True) \
+.add("ProductName", StringType(), True) \
+.add("Category", StringType(), True) \
+.add("ListPrice", DoubleType(), True)
+
+df = spark.read.format("csv").option("header","true").schema(schema).load("Files/product_sub/products.csv")
+# df now is a Spark DataFrame containing CSV data from "Files/products/products.csv".
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df.write.format("delta").saveAsTable("external_products", path="abfss://435bef55-2c35-40fd-9667-dcf68f447683@onelake.dfs.fabric.microsoft.com/0343156d-43b4-4187-be43-c658a0dfd404/Files")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df.write.format("delta").saveAsTable("managed_products")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
